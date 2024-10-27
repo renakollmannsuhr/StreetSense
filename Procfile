c14097a04c8ed910588ea23387ce080a8daea066
Rena Kollmann-Suhr <maxeen.kollmannsuhr14@gmail.com>
2024-10-27 16:03:17 -0700

    secured key and created procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn streetsense.wsgi --log-file -
--- streetsense/streetsense_backend/streetsense_backend/settings.py
+++ streetsense/streetsense_backend/streetsense_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+_3jv!cta0o2m!+3y*@8lv07%f&&zd)-l223r94j#p7&*qrb2'
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Reference the SECRET_KEY from the .env file
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
