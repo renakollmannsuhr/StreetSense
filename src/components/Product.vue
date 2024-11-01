60fb82743134cebbc8ea8cef707d7b860d43105f
Emila Suska <emila.suska@gmail.com>
2024-10-31 22:18:45 -0700

    created Product page

---
+++ src/components/Product.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="product">
+    <div class="logo-container">
+      <img src="/StreetSenseLogo.png" alt="StreetSense Logo" class="logo" />
+    </div>
+    <p>Fostering a sense of community for commuters and university students by offering real-time, community-reported crime alerts, empowering you to navigate your city confidently and make it home safely.</p>
+    <div class="screenshot-container">
+      <img src="/appview.png" alt="App View" class="screenshot" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductPage',
+}
+</script>
+
+<style scoped>
+.product {
+  padding: 20px;
+}
+
+.logo-container, .screenshot-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.logo {
+  height: 100px;
+  width: auto;
+  margin: 1rem;
+}
+
+.screenshot {
+  height: 450px;
+  width: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import VueGoogleMaps from '@fawmi/vue-google-maps';
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Product from './components/Product.vue'
+import StreetSense from './components/StreetSense.vue'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
-const app = createApp(App);
+const routes = [
+  {
+    path: '/',
+    name: 'StreetSense',
+    component: StreetSense
+  },
+  {
+    path: '/product',
+    name: 'ProductPage',
+    component: Product
+  }
+]
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
 app.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCQHMZcwOioIC_iF95ITHO3Uhn79aiJ2mM',  
-    libraries: 'places',  
+    key: 'AIzaSyCQHMZcwOioIC_iF95ITHO3Uhn79aiJ2mM',
+    libraries: 'places',
   },
-});
+})
 
-app.mount('#app');
+app.mount('#app')
