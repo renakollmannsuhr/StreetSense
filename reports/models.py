aec75f8f6c3db393b5e11cc3891a6a99f908194d
Rena Kollmann-Suhr <maxeen.kollmannsuhr14@gmail.com>
2024-11-28 10:39:15 -0800

    add vote_count column to crimereports table

--- reports/models.py
+++ reports/models.py
@@ -8,6 +8,7 @@ class CrimeReport(models.Model):
     longitude = models.FloatField()
     date_reported = models.DateTimeField(auto_now_add=True)
     date_occurred = models.DateTimeField(default=None, blank=True, null=True)
+    vote_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
