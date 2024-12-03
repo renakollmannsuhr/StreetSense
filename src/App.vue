0f7b9aebf46b08393f02fad9a1f1f4d716a3f6a3
Rena Kollmann-Suhr <maxeen.kollmannsuhr14@gmail.com>
2024-12-03 15:58:04 -0800

    add tooltips and fix button colouring

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,13 @@ export default {
   name: 'App',
 }
 </script>
+<style>
+.tippy-box[data-theme~='theme'] {
+  background-color: #1e3e80;
+  color: white;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+}
+.tippy-box[data-theme~='theme'][data-placement^='top'] > .tippy-arrow::before {
+  border-top-color: #1e3e80;
+}
+</style>
